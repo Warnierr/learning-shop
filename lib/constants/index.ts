dd653391c38fdd24e422fa9e3a071fd4157f2388
Warnierr <rww.warnier@gmail.com>
2024-07-05 14:02:27 +0200

    first step, home page

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Wakil Shop'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DEXCRIPTION ||
+  'An e-commerce platform built with Next.js, Postgres, Shadcn'
