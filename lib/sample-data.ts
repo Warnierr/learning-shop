5043a9b7b7e1a06f559f7599b9a5206bf15a5b83
Warnierr <rww.warnier@gmail.com>
2024-07-07 23:34:18 +0200

    images tests 1

---
+++ lib/sample-data.ts
@@ -0,0 +1,60 @@
+const sampleData = {
+  products: [
+    {
+      name: 'BRACELET PIERRES MIXTES',
+      slug: 'bracelet-pierres-mixtes',
+      category: 'Bracelet',
+      images: ['/assets/images/p1-1.jpg', '/assets/images/p1-2.jpg'],
+      price: '20.00',
+      brand: 'Wakil',
+      rating: '4.5',
+      numReviews: 10,
+      stock: 5,
+      description: 'Bracelet fait à partir de plusieurs type de roche.',
+      isFeatured: true,
+      banner: '/assets/images/banner-1.jpeg',
+    },
+    {
+      name: 'BRACELET PIERRE DE LUNE',
+      slug: 'bracelet-pierre-de-lune',
+      category: 'Bracelet',
+      images: ['/assets/images/p2-1.jpg', '/assets/images/p2-2.jpeg'],
+      price: '25.00',
+      brand: 'JB',
+      rating: '4.2',
+      numReviews: 8,
+      stock: 10,
+      description: 'Bracelet fait à partir de pierres roses',
+      isFeatured: true,
+      banner: '/assets/images/banner-2.jpeg',
+    },
+    {
+      name: 'BRACELET PIERRE ROCHEUSE',
+      slug: 'bracelet-pierre-rocheuse',
+      category: 'Wakil',
+      images: ['/assets/images/p3-1.jpg', '/assets/images/p3-2.jpg'],
+
+      price: '20.00',
+      brand: 'Nike',
+      rating: '4.9',
+      numReviews: 3,
+      stock: 0,
+      description:
+        'Bracelet fait à partir de pierres rocheuses ressemblant à des pierres météorites',
+    },
+    {
+      name: 'BRACELET PIERRES VIOLETTES',
+      slug: 'bracelet-pierres-violettes',
+      category: 'Wakil',
+      images: ['/assets/images/p4-1.jpg', '/assets/images/p4-2.jpg'],
+      price: '25.00',
+      brand: 'Lacoste',
+      rating: '3.6',
+      numReviews: 5,
+      stock: 10,
+      description: 'Bracelet fait à partir de pierres violettes',
+    },
+  ],
+}
+
+export default sampleData
