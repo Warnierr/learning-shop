e03fb3af1230f16c39679f783cf3c375c11c4b6d
Warnierr <rww.warnier@gmail.com>
2024-07-30 23:02:31 +0200

    Added new tables to manage user accounts, sessions, and verificationTokens.

--- db/schema.ts
+++ db/schema.ts
@@ -10,7 +10,72 @@ import {
   uuid,
 } from 'drizzle-orm/pg-core'
 
-// Defines the database schema for the 'products' table.
+import { primaryKey } from 'drizzle-orm/pg-core/primary-keys'
+import { AdapterAccountType } from 'next-auth/adapters'
+
+// USERS
+export const users = pgTable('user', {
+  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  name: text('name'),
+  email: text('email').notNull(),
+  role: text('role').notNull().default('user'),
+  password: text('password'),
+  emailVerified: timestamp('emailVerified', { mode: 'date' }),
+  image: text('image'),
+})
+
+// ACCOUNTS
+// Defines the table structure for user accounts, managing various authentication providers.
+export const accounts = pgTable(
+  'account',
+  {
+    userId: uuid('userId')
+      .notNull()
+      .references(() => users.id, { onDelete: 'cascade' }),
+    type: text('type').$type<AdapterAccountType>().notNull(),
+    provider: text('provider').notNull(),
+    providerAccountId: text('providerAccountId').notNull(),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
+    expires_at: integer('expires_at'),
+    token_type: text('token_type'),
+    scope: text('scope'),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+  })
+)
+
+// SESSIONS
+// Table to manage sessions for authenticated users, linking back to the user table.
+export const sessions = pgTable('session', {
+  sessionToken: text('sessionToken').primaryKey(),
+  userId: uuid('userId')
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+  expires: timestamp('expires', { mode: 'date' }).notNull(),
+})
+
+// VERIFICATION TOKENS
+// Defines a table for storing verification tokens for email or other authentication methods.
+export const verificationTokens = pgTable(
+  'verificationToken',
+  {
+    identifier: text('identifier').notNull(),
+    token: text('token').notNull(),
+    expires: timestamp('expires', { mode: 'date' }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
+  })
+)
+
+// PRODUCTS
+// Defines the database schema for the 'products' table with various product details.
 export const products = pgTable(
   'product', // The name of the table in the database.
   {
