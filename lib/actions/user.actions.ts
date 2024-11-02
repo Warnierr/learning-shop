963dec24ec8f23ec8edc1ac8a50bf00a6b690da8
Warnierr <rww.warnier@gmail.com>
2024-11-03 00:09:46 +0100

    signup functions, hash password, put into db

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,7 +4,46 @@
 // Import necessary functions from Next.js and authentication libraries
 import { isRedirectError } from 'next/dist/client/components/redirect'
 import { signIn, signOut } from '@/auth'
-import { signInFormSchema } from '../validator'
+import { signInFormSchema, signUpFormSchema } from '../validator'
+import { formatError } from '../utils'
+import { hashSync } from 'bcrypt-ts-edge'
+import db from '@/db/drizzle'
+import { users } from '@/db/schema'
+
+// USER
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    })
+    const values = {
+      id: crypto.randomUUID(),
+      ...user,
+      password: hashSync(user.password, 10),
+    }
+    await db.insert(users).values(values)
+    await signIn('credentials', {
+      email: user.email,
+      password: user.password,
+    })
+    return { success: true, message: 'User created successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+    return {
+      success: false,
+      message: formatError(error).includes(
+        'duplicate key value violates unique constraint "user_email_idx"'
+      )
+        ? 'Email is already exist'
+        : formatError(error),
+    }
+  }
+}
 
 // Define an asynchronous function to handle user sign-in with credentials
 export async function signInWithCredentials(
